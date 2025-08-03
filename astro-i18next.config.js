b3e714a26b1f4d226e62ce239871d384cb41bbe3
nvmhussain <jamesjames4301@gmail.com>
2025-08-04 01:55:18 +0500

    Update astro-i18next.config.js

--- astro-i18next.config.js
+++ astro-i18next.config.js
@@ -1,9 +1,7 @@
-/** @type {import('astro-i18next').AstroI18nextConfig} */
+/** @type {import('astro-i18next/types').UserConfig} */
 export default {
-  defaultLocale: "en",
-  locales: ["en", "it", "fr", "de", "es", "hi", "ar", "id", "ru", "pt", "ko", "tl", "nl", "ms", "tr"],
-
-
-
-  
+  defaultLocale: 'en',
+  locales: ['en', 'fr', 'es'],
+  routes: 'src/translations/routes.json', // Make sure this file exists
+  showDefaultLocale: false,
 };
